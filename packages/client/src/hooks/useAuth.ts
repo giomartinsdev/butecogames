3beb9d93a4606a04c8ea453ad8e6c85e52792fad
Giovanni Martins <giovannidealmeidamartins@gmail.com>
2026-02-24 23:46:52 -0300

    fix: remove todo

--- packages/client/src/hooks/useAuth.ts
+++ packages/client/src/hooks/useAuth.ts
@@ -8,10 +8,7 @@ export function useAuth() {
     session: session.data?.session ?? null,
     isLoading: session.isPending,
     isAuthenticated: !!session.data?.user,
-    signInWithDiscord: () => signIn.social({ 
-      provider: "discord", 
-      callbackURL: window.location.origin
-    }), // todo: later remove
+    signInWithDiscord: () => signIn.social({ provider: "discord" }),
     signOut: () => signOut(),
   };
 }
